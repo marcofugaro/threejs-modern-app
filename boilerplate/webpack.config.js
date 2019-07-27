71537ed4a2a7dee1497f74bf206b8b485d20cf5a
Marco Fugaro <marco.fugaro@gmail.com>
2019-07-27 10:54:24 +0200

    🔨  Enable HMR

--- boilerplate/webpack.config.js
+++ boilerplate/webpack.config.js
@@ -12,6 +12,7 @@ const EventHooksPlugin = require('event-hooks-webpack-plugin')
 const chalk = require('chalk')
 const indentString = require('indent-string')
 const _ = require('lodash')
+const webpack = require('webpack')
 
 const PROTOCOL = 'http'
 const HOST = '0.0.0.0'
@@ -103,15 +104,17 @@ module.exports = merge.smart(
       // Sssh...
       quiet: true,
       clientLogLevel: 'none',
-      // uncomment these lines to enable HMR
-      // hot: true,
-      // hotOnly: true,
+      // enable HMR
+      hot: true,
+      hotOnly: true,
       after() {
         // try to open into the already existing tab
         openBrowser(urls.localUrlForBrowser)
       },
     },
     plugins: [
+      // Needed to enable HMR
+      new webpack.HotModuleReplacementPlugin(),
       // Automatic rediscover of packages after `npm install`
       new WatchMissingNodeModulesPlugin('node_modules'),
       // TODO use webpack's api when it will be implemented
